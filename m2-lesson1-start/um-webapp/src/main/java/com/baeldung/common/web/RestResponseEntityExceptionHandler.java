4c13b597a3386ae0b9cdb4dedc6a63aeb7759155
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2020-06-02 14:10:59 -0500

    better exception message if description is null

--- m2-lesson1-start/um-webapp/src/main/java/com/baeldung/common/web/RestResponseEntityExceptionHandler.java
+++ m2-lesson1-start/um-webapp/src/main/java/com/baeldung/common/web/RestResponseEntityExceptionHandler.java
@@ -2,14 +2,17 @@ package com.baeldung.common.web;
 
 import com.baeldung.common.web.exception.ApiError;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -40,9 +43,16 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
                 request);
     }
 
+    @ExceptionHandler(value = DataIntegrityViolationException.class)
+    public final ResponseEntity<Object> handleBadRequest(final DataIntegrityViolationException ex,
+            final WebRequest request) {
+        return handleExceptionInternal(ex, message(HttpStatus.BAD_REQUEST, ex), new HttpHeaders(),
+                HttpStatus.BAD_REQUEST, request);
+    }
+
     private ApiError message(final HttpStatus httpStatus, final Exception ex) {
         final String message = ex.getMessage() == null ? ex.getClass().getSimpleName() : ex.getMessage();
-        final String devMessage = ex.getClass().getSimpleName();
+        final String devMessage = ExceptionUtils.getRootCauseMessage(ex);
 
         return new ApiError(httpStatus.value(), message, devMessage);
     }
