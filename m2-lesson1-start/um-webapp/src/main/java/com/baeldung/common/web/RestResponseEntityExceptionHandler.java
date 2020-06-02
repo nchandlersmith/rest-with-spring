68a401682ec5e46f85f619d3c4f35b61254a934e
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2020-06-02 13:47:07 -0500

    through end of lesson 1

--- m2-lesson1-start/um-webapp/src/main/java/com/baeldung/common/web/RestResponseEntityExceptionHandler.java
+++ m2-lesson1-start/um-webapp/src/main/java/com/baeldung/common/web/RestResponseEntityExceptionHandler.java
@@ -1,8 +1,16 @@
 package com.baeldung.common.web;
 
+import com.baeldung.common.web.exception.ApiError;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -17,4 +25,25 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     // API
 
     // 400
+    @Override
+    protected final ResponseEntity<Object> handleHttpMessageNotReadable(final HttpMessageNotReadableException ex,
+            final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        return handleExceptionInternal(ex, message(HttpStatus.BAD_REQUEST, ex), headers, HttpStatus.BAD_REQUEST,
+                request);
+    }
+
+    @Override
+    protected final ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
+            final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+
+        return handleExceptionInternal(ex, message(HttpStatus.BAD_REQUEST, ex), headers, HttpStatus.BAD_REQUEST,
+                request);
+    }
+
+    private ApiError message(final HttpStatus httpStatus, final Exception ex) {
+        final String message = ex.getMessage() == null ? ex.getClass().getSimpleName() : ex.getMessage();
+        final String devMessage = ex.getClass().getSimpleName();
+
+        return new ApiError(httpStatus.value(), message, devMessage);
+    }
 }
